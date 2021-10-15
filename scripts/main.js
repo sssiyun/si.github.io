ec67612d7ffd7ab6774224e6fff3c8ac095c901b
Si <osi@live.com>
2021-10-15 02:31:46 -0700

    update

--- scripts/main.js
+++ scripts/main.js
@@ -9,27 +9,3 @@ myImage.onclick = function () {
         myImage.setAttribute('src', 'images/bb1.jpg');
     }
 }
-
-//个性化欢迎语
-// let myButton = document.querySelector('button');
-// let myHeading = document.querySelector('h1');
-// function setUserName() {
-//     let myName = prompt('请输入你的名字。');
-//     if(!myName || myName === null) {
-//       setUserName();
-//     } else {
-//       localStorage.setItem('name', myName);
-//       myHeading.innerHTML = 'Nice to meet you, ' + myName;
-//     }
-//   }
-
-// if (!localStorage.getItem('name')) {
-//     setUserName();
-// } else {
-//     let storedName = localStorage.getItem('name');
-//     myHeading.textContent = 'Nice to meet you, ' + storedName;
-// }
-
-// myButton.onclick = function() {
-//     setUserName();
-//  }
